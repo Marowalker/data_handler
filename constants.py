f30e2c6828adaad0be161d8d349be9214465c57e
marowalker <binhnguyen2798@gmaill.com>
2023-01-11 08:38:11 +0700

    Initial commit

---
+++ constants.py
@@ -0,0 +1,14 @@
+DDI = 'DDICorpus/'
+DDI_TRAIN = DDI + 'Train/'
+DDI_TEST = DDI + 'Test/Test for DDI Extraction task/'
+
+DRUGBANK_TRAIN = DDI_TRAIN + 'DrugBank/'
+DRUGBANK_TEST = DDI_TEST + 'DrugBank/'
+MEDLINE_TRAIN = DDI_TRAIN + 'Medline/'
+MEDLINE_TEST = DDI_TEST + 'MedLine/'
+
+CHEMPROT = 'ChemProt_Corpus/'
+CHEMPROT_TRAIN = CHEMPROT + 'chemprot_training/'
+CHEMPROT_DEV = CHEMPROT + 'chemprot_development/'
+CHEMPROT_TEST = CHEMPROT + 'chemprot_test_gs/'
+CHEMPROT_EXAMPLE = CHEMPROT + 'chemprot_sample/'
